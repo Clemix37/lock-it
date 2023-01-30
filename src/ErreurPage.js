8b0cc17a9ca7bd029c73ef0dd8e10e198cbb45b7
H4ckW1s3r <clementix01@gmail.com>
2023-01-30 22:37:09 +0100

    Review error page

--- src/ErreurPage.js
+++ src/ErreurPage.js
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 function ErreurPage(){
+    const navigate = useNavigate();
+    const handleRevenirAcceuil = () => {
+        navigate('/');
+    };
     return (
         <div className="colonne">
-            BON JE CROIS QUE TU T'ES PERDU
+            <div className="ligne"><h1>ERREUR 404 - PAGE NON TROUVÉE</h1></div>
+            <div className="ligne">
+                C'est pas grave, tu sais ce qu'on va faire ? On va revenir à la page d'accueil ...
+            </div>
+            <div className="ligne">
+                <span className="span-btn btn" onClick={handleRevenirAcceuil}>Quelle bonne idée !</span>
+            </div>
         </div>
     )
 }
